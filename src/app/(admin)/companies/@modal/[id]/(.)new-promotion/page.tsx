039c8736ee77860ac0703ab7f7ecc0b370e7a123
DisRachik <radchenkoig@gmail.com>
2024-04-23 18:02:01 +0300

    fix: change next/router to next/navigation for Modal

--- src/app/(admin)/companies/@modal/[id]/(.)new-promotion/page.tsx
+++ src/app/(admin)/companies/@modal/[id]/(.)new-promotion/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import PromotionFormModal from '@/app/components/promotion-form-modal';
 
 export interface IPageProps {
